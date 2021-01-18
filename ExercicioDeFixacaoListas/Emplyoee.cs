a86a168de64ab667e0d8ed3e1d460e74fac7d24d
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:20:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDeFixacaoListas/Emplyoee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ExercicioDeFixacaoListas
+{
+    class Emplyoee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Emplyoee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", " 
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
